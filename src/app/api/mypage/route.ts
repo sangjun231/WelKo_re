f2467241d66e83417704d8129f49e69335ae84cf
unknown <choipoe@gmail.com>
2024-07-19 16:24:30 +0900

    Feat: 로그인,회원가입 기능 구현

--- src/app/api/mypage/route.ts
+++ src/app/api/mypage/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { createClient } from '@/supabase/server';
+import { createClient } from '@/utils/supabase/server';
 
 export async function GET(req: NextRequest) {
   const supabase = createClient();
