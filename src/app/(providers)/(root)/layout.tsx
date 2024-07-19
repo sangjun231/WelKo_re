16ad06c620d7e1e9b57d3c557a30850239808190
Yejin Choi <llhdf123@naver.com>
2024-07-19 19:44:36 +0900

    Feat: header, footer, up button 간략하게 설정

--- src/app/(providers)/(root)/layout.tsx
+++ src/app/(providers)/(root)/layout.tsx
@@ -1,11 +1,17 @@
-import { PropsWithChildren } from "react";
+import UpButton from '@/components/common/Button/UpButton';
+import Footer from '@/components/common/Footer/Footer';
+import Header from '@/components/common/Header/Header';
+import { PropsWithChildren } from 'react';
 
 function RootLayout({ children }: PropsWithChildren) {
   return (
-    <div id="root" className="flex flex-col max-w-[1440px] sm:max-w-[] min-h-screen mx-auto bg-black text-white">
-      <div className="flex flex-1 gap-4">
-        <main className="flex-1 p-6">
+    <div id="root">
+      <div>
+        <main>
+          <Header />
           {children}
+          <UpButton />
+          <Footer />
         </main>
       </div>
     </div>
---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,62 @@
+'use client';
+import { useEffect, useRef, useState } from 'react';
+
+function Header() {
+  const [getUser, setGetUser] = useState<boolean>(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsDropdownOpen(false);
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className="flex justify-between">
+        <h1>LOGO</h1>
+        {getUser ? (
+          <div ref={dropdownRef} className="relative mr-16 flex">
+            <button className="bg-gray-400 p-2">Writing</button>
+            <button
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              className="flex items-center space-x-2 rounded-md border-l-stone-400"
+            >
+              <p>MENU</p>
+              <p>profile</p>
+            </button>
+            {isDropdownOpen && (
+              <div className="absolute left-0 top-5 z-50 mt-4 w-48 rounded-md bg-gray-200 shadow-lg">
+                <ul className="py-1">
+                  <li>
+                    <button className="block w-full px-4 py-2 text-start text-sm text-black hover:bg-gray-300">
+                      마이페이지
+                    </button>
+                  </li>
+                  <li>
+                    <button className="block w-full px-4 py-2 text-start text-sm text-black hover:bg-gray-300">
+                      로그아웃
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        ) : (
+          <button onClick={() => setGetUser(!getUser)}>로그인 및 회원가입</button>
+        )}
+      </div>
+
+      <div>검색창</div>
+    </div>
+  );
+}
+
+export default Header;
