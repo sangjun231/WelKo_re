e700d346ea3220ca70f737886942c6e8885e4d17
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-07-20 15:18:07 +0900

    Design: detail page에서 작성자 부분 html 틀 제작

---
+++ src/app/(providers)/(root)/detail/_components/Guide.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Guide = () => {
+  return (
+    <div>
+      <h3>가이드 소개</h3>
+      <div>
+        <img />
+        <h5>이름</h5>
+      </div>
+      <button>메시지 보내기</button>
+    </div>
+  );
+};
