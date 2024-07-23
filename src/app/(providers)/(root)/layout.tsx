d0133e7fec06ed7f091e53abef3b0e9a667d3f4a
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-07-23 14:00:28 +0900

    Style: 최소 너비 지정

--- src/app/(providers)/(root)/layout.tsx
+++ src/app/(providers)/(root)/layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 function RootLayout({ children }: PropsWithChildren) {
   return (
-    <div id="root" className="flex flex-col max-w-[1440px] sm:max-w-[] min-h-screen mx-auto">
+    <div id="root" className="flex flex-col max-w-[1440px] sm:max-w-[360px] min-h-screen mx-auto">
       <div className="flex flex-1 gap-4">
         <main className="flex-1 p-6">
           {children}
