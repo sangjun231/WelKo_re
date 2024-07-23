39496606119ebed9e413086bc839bd0c2530266f
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-07-23 15:54:10 +0900

    Feat: 아이콘 변경 추가

--- src/app/(providers)/(root)/detail/_components/Likes.tsx
+++ src/app/(providers)/(root)/detail/_components/Likes.tsx
@@ -1,15 +1,20 @@
-import { FaRegHeart } from 'react-icons/fa';
+import { useState } from 'react';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
 
 export interface Heart {
   liked: boolean;
 }
 
 const Likes = () => {
+  const [liked, setLiked] = useState(false);
+
+  const handleLike = () => {
+    setLiked(!liked);
+  };
+
   return (
     <div>
-      <button>
-        <FaRegHeart size={30} />
-      </button>
+      <button onClick={handleLike}>{liked ? <FaHeart size={30} color="red" /> : <FaRegHeart size={30} />}</button>
     </div>
   );
 };
