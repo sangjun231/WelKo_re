16aa2a20018712b00ea04844ffc022180a5c1f9e
sangjunShin <987654321123sin@gmail.com>
2024-07-23 21:49:47 +0900

    Fix: 이미지 재업로드시 발생하던 반영이 이상한 버그 해결
    이미지 이름이 고정되어있으므로 기존 이미지를 삭제하여 새로운 이미지를 재등록해서 불러오도록 개선

--- src/app/(providers)/(root)/(mypage)/[id]/my-page/_components/ProfileForm.tsx
+++ src/app/(providers)/(root)/(mypage)/[id]/my-page/_components/ProfileForm.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { createClient } from '@/utils/supabase/client';
 import { API_MYPAGE_PROFILE } from '@/utils/apiConstants';
+import Image from 'next/image';
 
 type Profile = {
   id: string;
@@ -36,6 +37,13 @@ const ProfileForm = ({ userId }: { userId: string }) => {
       const fileName = `${userId}.${fileExt}`;
       const filePath = `profile_images/${fileName}`;
 
+      // 기존 파일 삭제
+      const existingImagePath = profile?.avatar?.split('/').pop();
+      if (existingImagePath && existingImagePath !== fileName) {
+        await supabase.storage.from('users').remove([`profile_images/${existingImagePath}`]);
+      }
+
+      // 새로운 파일 업로드
       const { error: uploadError } = await supabase.storage.from('users').upload(filePath, file, {
         upsert: true
       });
@@ -50,6 +58,14 @@ const ProfileForm = ({ userId }: { userId: string }) => {
         setImageUrl(publicUrlData.publicUrl);
 
         await axios.put(API_MYPAGE_PROFILE(userId), { id: userId, avatar: publicUrlData.publicUrl });
+
+        // 프로필 데이터를 다시 가져와서 상태를 업데이트
+        const response = await axios.get(API_MYPAGE_PROFILE(userId));
+        const profileData = response.data;
+        setProfile(profileData);
+        setNickname(profileData.name);
+        setEmail(profileData.email);
+        setImageUrl(publicUrlData.publicUrl);
       } else {
         console.error('Failed to get public URL');
       }
@@ -75,7 +91,15 @@ const ProfileForm = ({ userId }: { userId: string }) => {
       {profile ? (
         <form onSubmit={handleSubmit}>
           <div>
-            <img src={imageUrl} alt="Profile" className="h-[76px] w-[76px] rounded-full" />
+            {imageUrl && (
+              <Image
+                src={`${imageUrl}?${new Date().getTime()}`}
+                alt="Profile"
+                width={70}
+                height={70}
+                className="rounded-full"
+              />
+            )}
             <input type="file" onChange={handleImageChange} />
           </div>
           <input
