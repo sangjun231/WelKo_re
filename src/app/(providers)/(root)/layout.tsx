ce7c8432f741cbbf7ab0bc3e86d64ae283e4f886
namsui26 <skatnqls0122@naver.com>
2024-07-23 14:14:52 +0900

    Fix:실수로 없앤 코드 다시 추가

--- src/app/(providers)/(root)/layout.tsx
+++ src/app/(providers)/(root)/layout.tsx
@@ -8,6 +8,7 @@ function RootLayout({ children }: PropsWithChildren) {
     <div id="root" className="flex flex-col max-w-[1440px] sm:max-w-[360] min-h-screen mx-auto">
       <div className="flex flex-1 gap-4">
         <main className="flex-1 p-6">
+          <Header />
           {children}
           <UpButton />
           <Footer />
