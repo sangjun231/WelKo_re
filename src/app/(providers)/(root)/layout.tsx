c6acdbce0d04164e95610426ecefe4f5d6ba41b9
sangjunShin <987654321123sin@gmail.com>
2024-07-23 14:47:21 +0900

    Refactor: 마이페이지 route.ts id폴더 적용 -> 프로필 및 리뷰 본인 계정만 열람 및 수정 가능
    apiConstants.ts 수정으로 api호출관련도 수정됨

--- src/app/(providers)/(root)/layout.tsx
+++ src/app/(providers)/(root)/layout.tsx
@@ -1,12 +1,10 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren } from 'react';
 
 function RootLayout({ children }: PropsWithChildren) {
   return (
-    <div id="root" className="flex flex-col max-w-[1440px] sm:max-w-[] min-h-screen mx-auto">
+    <div id="root" className="mx-auto flex min-h-screen max-w-[1440px] flex-col sm:max-w-[360px]">
       <div className="flex flex-1 gap-4">
-        <main className="flex-1 p-6">
-          {children}
-        </main>
+        <main className="flex-1 p-6">{children}</main>
       </div>
     </div>
   );
