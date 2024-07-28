4a5c255640910af65954e274b8241af2039c5495
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-07-29 00:08:24 +0900

    Feat: 환불 시 supabase에서 해당 결제 데이터 삭제하는 로직으로 변경

--- src/app/api/detail/payment/[id]/route.ts
+++ src/app/api/detail/payment/[id]/route.ts
@@ -53,14 +53,14 @@ export async function POST(request: NextRequest, { params }: { params: { id: str
       return NextResponse.json({ message: 'Refund failed', error: responseData.reason }, { status: 400 });
     } else {
       // 환불 성공 시 처리 (예: 데이터베이스 업데이트)
-      const { error: cancelError } = await supabase.from('payments').update({ pay_state: 'cancel' }).eq('id', id);
+      const { error: cancelError } = await supabase.from('payments').delete().eq('id', id);
 
       if (cancelError) {
         console.error('Error saving cancel data:', cancelError);
         return NextResponse.json({ error: 'Error saving cancel data' }, { status: 500 });
       }
 
-      return NextResponse.json({ message: 'Cancel successful', data: responseData }, { status: 200 });
+      return NextResponse.json({ message: '환불 성공!', data: responseData }, { status: 200 });
     }
   } catch (error) {
     const axiosError = error as AxiosError;
