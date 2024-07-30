72c75440a8eba6bd7a7f430d0300aac7689dc90e
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-07-30 23:20:06 +0900

    Feat: 로그인 안한 사용자는 홈아이콘을 제외한 다른 아이콘 클릭시 로그인 페이지로 이동

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -15,13 +15,29 @@ function Navbar() {
   const setSelectedComponent = useMyPageStore((state) => state.setSelectedComponent);
 
   const handleReservationsClick = () => {
+    if (!user) {
+      router.push('/login');
+      return;
+    }
     setSelectedComponent('Reservations');
     router.push(`/${user?.id}/mypage`);
   };
 
   const handleLikesClick = () => {
+    if (!user) {
+      router.push('/login');
+      return;
+    }
     setSelectedComponent('likes');
     router.push(`/${user?.id}/mypage`);
+  };
+
+  const handleMessagesClick = () => {
+    if (!user) {
+      router.push('/login');
+      return;
+    }
+    router.push(`/${user?.id}/chatpage`);
   };
 
   return (
@@ -41,16 +57,16 @@ function Navbar() {
           <span className="text-[10px]">Reservations</span>
         </div>
         <Link href="/postpage">
-          <div className="flex flex-col items-center">
-            <BsFillPlusCircleFill size={40} style={{ color: '#BA68C8' }} className="" />
-          </div>
+          <Link href="/postpage">
+            <div className="flex flex-col items-center rounded-full shadow-[0px_8px_19px_rgba(0,0,0,0.17)]">
+              <BsFillPlusCircleFill size={40} style={{ color: '#BA68C8' }} />
+            </div>
+          </Link>
         </Link>
-        <Link href={`/${user?.id}/chatpage`}>
-          <div className="flex flex-col items-center hover:text-[#BA68C8]">
-            <AiOutlineMessage size={24} />
-            <span className="text-[10px]">Messages</span>
-          </div>
-        </Link>
+        <div onClick={handleMessagesClick} className="flex cursor-pointer flex-col items-center hover:text-[#BA68C8]">
+          <AiOutlineMessage size={24} />
+          <span className="text-[10px]">Messages</span>
+        </div>
         <div onClick={handleLikesClick} className="flex flex-col items-center hover:text-[#BA68C8]">
           <BsPersonCircle size={24} />
           <span className="text-[10px]">Mypage</span>
