b4c8350c39b6669699311b112cb5db558ea8344c
sangjunShin <987654321123sin@gmail.com>
2024-07-30 15:24:19 +0900

    Feat: 내 투어 일정 별로 분류 및 작성 기준 최신순 정렬 적용
    세부 UI 조정 및 기능 연결 예정

--- src/app/(providers)/(root)/(mypage)/[id]/mypage/_components/PostList.tsx
+++ src/app/(providers)/(root)/(mypage)/[id]/mypage/_components/PostList.tsx
@@ -4,9 +4,9 @@ import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
 import React, { useEffect } from 'react';
 import { useParams } from 'next/navigation';
-import { API_MYPAGE_POST } from '@/utils/apiConstants';
 import Link from 'next/link';
 import Image from 'next/image';
+import { API_MYPAGE_POST } from '@/utils/apiConstants';
 import { Tables } from '@/types/supabase';
 
 export default function PostList() {
@@ -16,9 +16,10 @@ export default function PostList() {
   const getPostsData = async () => {
     try {
       const response = await axios.get(API_MYPAGE_POST(userId));
-      // 필터링 로직을 클라이언트에서 처리
       const data: Tables<'posts'>[] = response.data;
-      return data.filter((post) => post.user_id === userId);
+      return data
+        .filter((post) => post.user_id === userId)
+        .sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
     } catch (error) {
       if (axios.isAxiosError(error)) {
         throw new Error(`HTTP error! status: ${error.response?.status}`);
@@ -33,6 +34,20 @@ export default function PostList() {
     queryFn: getPostsData,
     enabled: !!userId
   });
+
+  const formatPrice = (price: number | null): string => {
+    if (price === null) {
+      return 'N/A';
+    }
+    return `$${price.toLocaleString('en-US')}`;
+  };
+
+  const tourStatus = (endDate: string | null): string => {
+    if (!endDate) return 'N/A';
+    const currentDate = new Date();
+    const tourEndDate = new Date(endDate);
+    return tourEndDate < currentDate ? '투어 완료' : '투어 예정';
+  };
 
   useEffect(() => {
     refetch();
@@ -49,24 +64,39 @@ export default function PostList() {
   }
 
   return (
-    <div className="max-w-[360px]">
-      {data.map((post) => (
-        <div key={post.id}>
-          <p>{new Date(post.created_at).toLocaleString()}</p>
-          <Link href={`/detail/${post.id}`}>
-            <div className="flex">
-              <Image
-                className="mb-[20px] mr-2"
-                src={post.image ?? '/icons/upload.png'}
-                alt={post.title ?? 'Default title'}
-                width={76}
-                height={76}
-              />
-              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-[14px] font-bold">{post.title}</p>
+    <div className="mb-10 max-w-[360px]">
+      {data.map((post) => {
+        const status = tourStatus(post.endDate);
+
+        return (
+          <div key={post.id} className="mb-4 border-b pb-4">
+            <p className="text-[15px]">{status}</p>
+            <p className="text-[15px]">작성 시각 : {new Date(post.created_at).toLocaleString()}</p>
+            <Link href={`/detail/${post.id}`}>
+              <div className="flex">
+                <Image
+                  src={post.image ?? '/icons/upload.png'}
+                  alt={post.title ?? 'Default title'}
+                  width={76}
+                  height={76}
+                />
+                <div className="ml-2 flex flex-col">
+                  <p className="overflow-hidden text-ellipsis whitespace-nowrap text-[15px] font-bold">{post.title}</p>
+                  <p className="text-[13px]">
+                    {post.startDate} -{post.endDate}
+                  </p>
+                  <p className="text-[13px]">{formatPrice(post.price)}</p>
+                </div>
+              </div>
+            </Link>
+            <div className="mt-2 flex space-x-2">
+              <button className="flex-1 rounded-lg border p-2">투어 수정</button>
+              <button className="flex-1 rounded-lg border p-2">투어 삭제</button>
             </div>
-          </Link>
-        </div>
-      ))}
+            {status === '투어 예정' && <button className="mt-2 w-full rounded-lg border p-2">예약자 확인</button>}
+          </div>
+        );
+      })}
     </div>
   );
 }
