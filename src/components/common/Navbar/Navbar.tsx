638edbd9532782da9411a133302939889c09b622
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-07-31 00:14:39 +0900

    Feat: alert 추가 및 특정 경로에서 nav바 안보이게하는 로직 작성

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -7,15 +7,17 @@ import { BsFillPlusCircleFill, BsPersonCircle } from 'react-icons/bs';
 import Link from 'next/link';
 import useAuthStore from '@/zustand/bearsStore';
 import { useMyPageStore } from '@/zustand/mypageStore';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 function Navbar() {
   const router = useRouter();
+  const pathname = usePathname();
   const user = useAuthStore((state) => state.user);
   const setSelectedComponent = useMyPageStore((state) => state.setSelectedComponent);
 
   const handleReservationsClick = () => {
     if (!user) {
+      alert('로그인이 필요한 서비스입니다!!');
       router.push('/login');
       return;
     }
@@ -25,6 +27,7 @@ function Navbar() {
 
   const handleLikesClick = () => {
     if (!user) {
+      alert('로그인이 필요한 서비스입니다!!');
       router.push('/login');
       return;
     }
@@ -34,40 +37,51 @@ function Navbar() {
 
   const handleMessagesClick = () => {
     if (!user) {
+      alert('로그인이 필요한 서비스입니다!!');
       router.push('/login');
       return;
     }
     router.push(`/${user?.id}/chatpage`);
   };
 
+  // 특정 경로에서 Navbar를 숨기기
+  const excludedRoutes = ['/login','/postpage'];
+  if (excludedRoutes.includes(pathname) || pathname.startsWith('/detail')) {
+    return null;
+  }
+
   return (
     <nav className="flex border-t border-[#DFDFE0] text-[#7A7A7A]">
-      <div className="container mx-auto hidden items-center justify-between p-8 sm:flex">
+      <div className="container mx-auto flex items-center justify-between p-8">
         <Link href="/">
-          <div className="flex flex-col items-center hover:text-[#BA68C8]">
+          <div className="flex flex-col items-center space-y-2 hover:text-[#BA68C8]">
             <RiHome3Line size={24} />
             <span className="text-[10px]">Home</span>
           </div>
         </Link>
         <div
           onClick={handleReservationsClick}
-          className="flex cursor-pointer flex-col items-center hover:text-[#BA68C8]"
+          className="flex cursor-pointer flex-col items-center space-y-2 hover:text-[#BA68C8]"
         >
           <IoCalendarOutline size={24} />
           <span className="text-[10px]">Reservations</span>
         </div>
         <Link href="/postpage">
-          <Link href="/postpage">
-            <div className="flex flex-col items-center rounded-full shadow-[0px_8px_19px_rgba(0,0,0,0.17)]">
-              <BsFillPlusCircleFill size={40} style={{ color: '#BA68C8' }} />
-            </div>
-          </Link>
+          <div className="flex flex-col items-center rounded-full shadow-[0px_8px_19px_rgba(0,0,0,0.17)]">
+            <BsFillPlusCircleFill size={50} style={{ color: '#BA68C8' }} />
+          </div>
         </Link>
-        <div onClick={handleMessagesClick} className="flex cursor-pointer flex-col items-center hover:text-[#BA68C8]">
+        <div
+          onClick={handleMessagesClick}
+          className="flex cursor-pointer flex-col items-center space-y-2 hover:text-[#BA68C8]"
+        >
           <AiOutlineMessage size={24} />
           <span className="text-[10px]">Messages</span>
         </div>
-        <div onClick={handleLikesClick} className="flex flex-col items-center hover:text-[#BA68C8]">
+        <div
+          onClick={handleLikesClick}
+          className="flex cursor-pointer flex-col items-center space-y-2 hover:text-[#BA68C8]"
+        >
           <BsPersonCircle size={24} />
           <span className="text-[10px]">Mypage</span>
         </div>
