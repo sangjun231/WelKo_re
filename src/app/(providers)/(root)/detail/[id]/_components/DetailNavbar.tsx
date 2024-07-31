29fe366ff81b2eb3c328e60703f160e95d227647
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-08-01 00:24:18 +0900

    Style: nav바 스타일 수정

--- src/app/(providers)/(root)/detail/[id]/_components/DetailNavbar.tsx
+++ src/app/(providers)/(root)/detail/[id]/_components/DetailNavbar.tsx
@@ -23,22 +23,23 @@ const DetailNavbar = () => {
   };
   return (
     <div className="fixed bottom-0 left-0 z-10 w-full bg-white">
-      <hr className="bg-grayscale-100 mx-auto h-[1px] w-full max-w-[360px]" />
-      <div className="mx-auto flex max-w-[320px] items-center justify-between py-4">
-        <div className="flex flex-col justify-center">
-          <div className="flex items-center text-lg">
-            <span className="text-primary-300 font-semibold">${post?.price}</span>
-            <span className="text-grayscale-600 font-medium">/Person</span>
+      <div className="shadow-custom-navbar border-t-1 border-grayscale-100 mx-auto max-w-[360px] py-4">
+        <div className="mx-auto flex max-w-[320px] items-center justify-between">
+          <div className="flex flex-col justify-center">
+            <div className="flex items-center text-lg">
+              <span className="text-primary-300 font-semibold">${post?.price}</span>
+              <span className="text-grayscale-600 font-medium">/Person</span>
+            </div>
+            <div className="flex justify-start">
+              <p className="text-grayscale-900 text-xs font-medium">
+                {formatDateRange(post?.startDate ?? null, post?.endDate ?? null)}
+              </p>
+            </div>
           </div>
-          <div className="flex justify-start">
-            <p className="text-grayscale-900 text-xs font-medium">
-              {formatDateRange(post?.startDate ?? null, post?.endDate ?? null)}
-            </p>
-          </div>
+          <button onClick={handleButtonClick} className="bg-primary-300 w-40 rounded-xl px-4 py-2 text-white">
+            Reserve
+          </button>
         </div>
-        <button onClick={handleButtonClick} className="bg-primary-300 w-40 rounded-xl px-4 py-2 text-white">
-          Reserve
-        </button>
       </div>
     </div>
   );
