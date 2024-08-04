aed465990b31d4df78016c10fbbdf3919b9101ed
sangjunShin <987654321123sin@gmail.com>
2024-08-04 17:22:29 +0900

    Design: 채팅 리스트 페이지 디자인 적용

--- src/app/(providers)/(root)/(mypage)/[id]/chatlistpage/_components/ChatList.tsx
+++ src/app/(providers)/(root)/(mypage)/[id]/chatlistpage/_components/ChatList.tsx
@@ -139,36 +139,42 @@ const ChatList = ({ userId }: ChatListProps) => {
           const isNewMessage = !newMessages[chatId] && firstMessage.sender_id !== userId;
 
           return (
-            <div className="mb-4" key={index} onClick={() => handleChatClick(chat)}>
-              {postDetails && (
-                <div className="flex items-center">
+            <div className="mb-[32px]" key={index} onClick={() => handleChatClick(chat)}>
+              {postDetails && senderDetails && (
+                <div className="flex">
                   <Image
-                    className="mr-4"
+                    className="rounded"
                     src={postDetails.image || '/icons/upload.png'}
                     alt={postDetails.title || 'Default name'}
-                    width={40}
-                    height={40}
+                    width={64}
+                    height={64}
+                    style={{ width: '64px', height: '64px' }}
                   />
-                  <p>{postDetails.title}</p>
+                  <div className="ml-[8px] flex w-full flex-col gap-[5px]">
+                    <div className="flex items-center justify-between">
+                      <p className="text-[13px] font-medium">{postDetails.title}</p>
+                      <p className="text-[10px] text-grayscale-500">
+                        {new Date(firstMessage?.created_at).toLocaleString()}
+                      </p>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <p className="text-[12px]">{firstMessage?.content}</p>
+                      {isNewMessage && <span className="w-[8px] h-[8px] rounded-full  bg-action-color"></span>}
+                    </div>
+                    <div className="flex">
+                      <Image
+                        className="items-center rounded-full"
+                        src={senderDetails.avatar || '/icons/upload.png'}
+                        alt={senderDetails.name || 'Default name'}
+                        width={16}
+                        height={16}
+                        style={{ width: '16px', height: '16px' }}
+                      />
+                      <p className="ml-[4px] text-[10px] text-grayscale-500">{senderDetails.name}</p>
+                    </div>
+                  </div>
                 </div>
               )}
-              {senderDetails && (
-                <div className="flex items-center">
-                  <Image
-                    className="mr-4"
-                    src={senderDetails.avatar || '/icons/upload.png'}
-                    alt={senderDetails.name || 'Default name'}
-                    width={40}
-                    height={40}
-                  />
-                  <p>{senderDetails.name}</p>
-                </div>
-              )}
-              <div className="flex justify-between">
-                <p>{firstMessage?.content}</p>
-                {isNewMessage && <span className="text-red-600">New</span>}
-              </div>
-              <p>{new Date(firstMessage?.created_at).toLocaleString()}</p>
             </div>
           );
         })}
--- src/app/(providers)/(root)/(mypage)/[id]/chatlistpage/page.tsx
+++ src/app/(providers)/(root)/(mypage)/[id]/chatlistpage/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useParams, useRouter } from 'next/navigation';
-
+import Image from 'next/image';
 import ChatList from './_components/ChatList';
 
 const MyPage = () => {
@@ -13,12 +13,15 @@ const MyPage = () => {
   };
 
   return (
-    <div className='mx-[20px] mt-[56px]'>
-      <p className="mb-4">ChatListPage</p>
-      <button onClick={handleBack}>Go Back</button>
-      <div>
-        <ChatList userId={id} />
+    <div className="mx-[20px] mt-[56px]">
+      <div className="relative mb-[36px] flex items-center justify-between">
+        <button className="rounded-[24px] bg-grayscale-50" onClick={handleBack}>
+          <Image src="/icons/tabler-icon-chevron-left.svg" alt="Go Back" width={32} height={32} />
+        </button>
+        <p className="absolute left-1/2 -translate-x-1/2 transform text-[18px] font-semibold">Message</p>
+        <div className="w-8"></div>
       </div>
+      <ChatList userId={id} />
     </div>
   );
 };
