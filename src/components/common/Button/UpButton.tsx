41befb42c9015f728f4b650b0b413314bfafa08f
Yejin Choi <llhdf123@naver.com>
2024-08-05 11:54:09 +0900

    Fix: 페이지에 따라 upbutton 위치 조정

--- src/components/common/Button/UpButton.tsx
+++ src/components/common/Button/UpButton.tsx
@@ -4,6 +4,8 @@ import { IoIosArrowRoundUp } from 'react-icons/io';
 
 function UpButton() {
   const [toggleButton, setToggleButton] = useState(false);
+  const [buttonPosition, setButtonPosition] = useState('bottom-32 right-3');
+
   const handleScroll = () => {
     const { scrollY } = window;
     scrollY > 200 ? setToggleButton(true) : setToggleButton(false);
@@ -12,24 +14,29 @@ function UpButton() {
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
 
+    const pathname = window.location.pathname;
+    if (pathname.startsWith('/detail')) {
+      setButtonPosition('bottom-20 right-3'); // detail 페이지에서의 위치
+    } else {
+      setButtonPosition('bottom-32 right-3'); // 기본 위치
+    }
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [window.location.pathname]);
 
   const goToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   return toggleButton ? (
-    <div>
-      <button
-        onClick={goToTop}
-        className="fixed bottom-16 right-4 flex h-11 w-11 items-center justify-center rounded-full border border-black bg-white"
-      >
-        <IoIosArrowRoundUp className="size-8" />
-      </button>
-    </div>
+    <button
+      onClick={goToTop}
+      className={`fixed ${buttonPosition} flex h-11 w-11 items-center justify-center rounded-full border border-black bg-white`}
+    >
+      <IoIosArrowRoundUp className="size-8" />
+    </button>
   ) : null;
 }
 export default UpButton;
