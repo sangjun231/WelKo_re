ca556237ebe32a5adaf1d582c4b3a7a73616937a
sangjunShin <987654321123sin@gmail.com>
2024-08-11 15:52:23 +0900

    Feat: 채팅 페이지 날짜별 채팅 나오도록 구현
    시각은 시:분이 메시지에 같이 나오도록 구현

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { fetchMessages, sendMessage } from '../services/chatService';
 import Image from 'next/image';
+import { format } from 'date-fns';
 
 type User = {
   id: string;
@@ -39,6 +40,18 @@ const Chat: React.FC<ChatProps> = ({ senderId, receiverId, postId }) => {
     }
   };
 
+  const groupedMessages = messages.reduce(
+    (groups, msg) => {
+      const date = format(new Date(msg.created_at), 'MMMM dd, yyyy');
+      if (!groups[date]) {
+        groups[date] = [];
+      }
+      groups[date].push(msg);
+      return groups;
+    },
+    {} as Record<string, Message[]>
+  );
+
   useEffect(() => {
     const fetchData = async () => {
       const fetchedMessages = await fetchMessages(senderId, receiverId, postId);
@@ -52,38 +65,47 @@ const Chat: React.FC<ChatProps> = ({ senderId, receiverId, postId }) => {
   return (
     <div className="flex h-screen flex-col text-[14px]">
       <div className="mb-[16px] overflow-y-auto border-b">
-        {messages.map((msg) => (
-          <div
-            key={msg.id}
-            className={`mb-10 flex flex-col ${msg.sender_id === senderId ? 'items-end' : 'items-start'}`}
-          >
-            {msg.sender_id !== senderId && msg.sender && (
-              <div className="flex items-center">
-                <Image
-                  className="mr-2 rounded-full"
-                  src={msg.sender.avatar}
-                  alt="avatar"
-                  width={44}
-                  height={44}
-                  style={{ width: '44px', height: '44px' }}
-                />
-                <p className="text-[13px] font-semibold">{msg.sender.name}</p>
+        {Object.entries(groupedMessages).map(([date, msgs]) => (
+          <div key={date}>
+            <p className="text-center text-[10px] text-grayscale-500">{date}</p>
+            {msgs.map((msg) => (
+              <div
+                key={msg.id}
+                className={`mb-10 flex flex-col ${msg.sender_id === senderId ? 'items-end' : 'items-start'}`}
+              >
+                {msg.sender_id !== senderId && msg.sender && (
+                  <div className="flex items-center">
+                    <Image
+                      className="mr-2 rounded-full"
+                      src={msg.sender.avatar}
+                      alt="avatar"
+                      width={44}
+                      height={44}
+                      style={{ width: '44px', height: '44px' }}
+                    />
+                    <p className="text-[13px] font-semibold">{msg.sender.name}</p>
+                  </div>
+                )}
+                <div className="flex items-end">
+                  <p
+                    className={`${
+                      msg.sender_id === senderId ? 'mr-[8px]' : 'order-1 ml-[8px]'
+                    } mt-[4px] text-[10px] text-grayscale-500`}
+                  >
+                    {format(new Date(msg.created_at), 'HH:mm')}
+                  </p>
+                  <div
+                    className={`mt-[7px] max-w-[240px] break-all px-[8px] py-[12px] ${
+                      msg.sender_id === senderId
+                        ? 'rounded-br-0 rounded-bl-[16px] rounded-tl-[16px] rounded-tr-[16px] bg-primary-50'
+                        : 'rounded-bl-0 rounded-br-[16px] rounded-tl-[16px] rounded-tr-[16px] bg-grayscale-50'
+                    }`}
+                  >
+                    <p className="text-[14px]">{msg.content}</p>
+                  </div>
+                </div>
               </div>
-            )}
-            <div
-              className={`mt-[7px] max-w-[240px] break-all px-[8px] py-[12px] ${
-                msg.sender_id === senderId
-                  ? 'rounded-br-0 rounded-bl-[16px] rounded-tl-[16px] rounded-tr-[16px] bg-primary-50'
-                  : 'rounded-bl-0 rounded-br-[16px] rounded-tl-[16px] rounded-tr-[16px] bg-grayscale-50'
-              }`}
-            >
-              <p className="text-[14px]">{msg.content}</p>
-            </div>
-            <p
-              className={` ${msg.sender_id === senderId ? 'right-0' : 'left-0'} mt-[4px] text-[10px] text-grayscale-500`}
-            >
-              {new Date(msg.created_at).toLocaleString()}
-            </p>
+            ))}
           </div>
         ))}
       </div>
