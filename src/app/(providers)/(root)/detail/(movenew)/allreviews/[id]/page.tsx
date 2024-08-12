7f7cbc94195bb769942e12fa3309e084c09c6db1
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-08-12 20:13:50 +0900

    Design: 리뷰 관련 컴포넌트와 페이지에서 아이콘을 svg 파일로 변경

--- src/app/(providers)/(root)/detail/(movenew)/allreviews/[id]/page.tsx
+++ src/app/(providers)/(root)/detail/(movenew)/allreviews/[id]/page.tsx
@@ -5,7 +5,7 @@ import { useParams } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import { fetchReviews, Review } from '@/utils/supabase/api/detail/reviews';
 import { averageRatings, formatRelativeDate } from '@/utils/detail/functions';
-import { FaStar } from 'react-icons/fa6';
+import IconStar from '/public/icons/tabler-icon-star-filled.svg';
 import BackButton from '@/components/common/Button/BackButton';
 
 const AllReviewsPage = () => {
@@ -42,17 +42,17 @@ const AllReviewsPage = () => {
         <div className="my-8">
           <BackButton />
         </div>
-        <div className="text-grayscale-900 ml-1 flex items-center gap-1 text-lg font-semibold">
-          <FaStar size={24} />
+        <div className="ml-1 flex items-center gap-1 text-lg font-semibold text-grayscale-900">
+          <IconStar alt="Reviews Star" width={24} height={24} />
           <h2 className="flex font-semibold">{averageRating.toFixed(2)}</h2>
           <span className="flex font-medium">·</span>
           <span className="flex font-medium">{reviews.length} reviews</span>
         </div>
         <div className="mt-5 flex flex-col items-center">
           {reviews.map((review) => (
-            <div key={review.id} className="bg-grayscale-50 mb-4 w-full items-center gap-3 rounded-2xl p-4">
+            <div key={review.id} className="mb-4 w-full items-center gap-3 rounded-2xl bg-grayscale-50 p-4">
               <div className="flex items-center gap-2 pb-2">
-                <FaStar size={16} />
+                <IconStar alt="Reviews Star" width={16} height={16} />
                 <p className="font-semibold">{review.rating.toFixed(2)}</p>
                 <p className="text-xs font-medium">{review.user_name}</p>
                 <p className="text-[10px] font-normal">{formatRelativeDate(review.created_at)}</p>
