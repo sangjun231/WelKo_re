e736d04dc3d9d5da17898cd9c2d562e11a15d951
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-08-13 22:20:17 +0900

    Feat: 엔드포인트 커스텀

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        mobile: '480px',
+        web: '481px'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
