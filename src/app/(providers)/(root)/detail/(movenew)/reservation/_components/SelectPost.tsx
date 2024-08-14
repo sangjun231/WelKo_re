d3e61ce2f170d24d7b1eb28dead67d4c5b920a0e
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-08-14 15:04:30 +0900

    Design: 디테일한 여백 수정

--- src/app/(providers)/(root)/detail/(movenew)/reservation/_components/SelectPost.tsx
+++ src/app/(providers)/(root)/detail/(movenew)/reservation/_components/SelectPost.tsx
@@ -43,7 +43,7 @@ const SelectPost = () => {
   if (!post) return <div className="flex items-center justify-center">Loading...</div>;
 
   return (
-    <div className="web:mt-[20px] web:justify-between mt-2 flex flex-col items-center gap-2">
+    <div className="web:mt-10 web:justify-between mt-2 flex flex-col items-center gap-2">
       <div className="mb-4 flex w-full items-center justify-between py-4">
         <BackButton className="web:hidden" />
         <h2 className="mobile:flex-grow web:text-left web:text-3xl text-center text-lg font-semibold">Pay</h2>
