dd41d7b612a71a6f04b70869ec4a1b103acc6262
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-08-14 14:47:56 +0900

    Chore: 불필요한 부분 제거

--- src/app/(providers)/(root)/layout.tsx
+++ src/app/(providers)/(root)/layout.tsx
@@ -5,7 +5,7 @@ import RouteChangeHandler from './(mainpage)/_components/RouteChangeHandler';
 
 function RootLayout({ children }: PropsWithChildren) {
   return (
-    <div id="root" className="web:max-w-[1440px] mobile:max-w-[360px] mx-auto flex min-h-screen flex-col">
+    <div id="root" className="web:max-w-[1440px] mx-auto flex min-h-screen max-w-[360px] flex-col">
       <Suspense fallback={<div>Loading...</div>}>
         <RouteChangeHandler />
       </Suspense>
