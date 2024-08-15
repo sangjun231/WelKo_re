b93e77cb06b50c5dc07b57a3e6b23fd10c26e6c2
sangjunShin <987654321123sin@gmail.com>
2024-08-16 03:11:47 +0900

    Design: 예약자 명단 페이지 반응형 적용 중

--- src/app/(providers)/(root)/(mypage)/[id]/mypage/tourreservationlistpage/_components/TourReservationList.tsx
+++ src/app/(providers)/(root)/(mypage)/[id]/mypage/tourreservationlistpage/_components/TourReservationList.tsx
@@ -87,19 +87,20 @@ const TourReservationList = ({ userId, postId }: { userId: string; postId: strin
       </div>
       <div className="mt-[24px] flex">
         <Image
-          className="rounded-[8px]"
+          className="h-[80px] w-[80px] rounded-[8px] web:h-[120px] web:w-[120px] web:rounded-[12px]"
           src={data[0].posts.image || '/default-image.png'}
           alt={data[0]?.posts.title}
           width={80}
           height={80}
-          style={{ width: '80px', height: '80px' }}
         />
-        <div className="ml-[8px]">
-          <p className="line-clamp-1 text-[14px] font-semibold text-primary-900">{data[0]?.posts.title}</p>
-          <p className="text-[14px] text-grayscale-500">
+        <div className="ml-[8px] flex flex-col gap-[4px] web:ml-[16px] web:gap-[8px]">
+          <p className="line-clamp-1 text-[14px] font-semibold text-primary-900 web:text-[21px]">
+            {data[0]?.posts.title}
+          </p>
+          <p className="text-[14px] text-grayscale-500 web:text-[18px]">
             {formatDateRange(data[0]?.posts.startDate, data[0]?.posts.endDate)}
           </p>
-          <p className="text-[13px] font-medium text-gray-700">
+          <p className="text-[13px] font-medium text-gray-700 web:text-[18px]">
             <span className="font-semibold text-primary-300">{formatPrice(data[0]?.posts.price)}</span>
             /Person
           </p>
@@ -107,34 +108,36 @@ const TourReservationList = ({ userId, postId }: { userId: string; postId: strin
       </div>
       <div>
         {data.map((reservation) => (
-          <div className="my-[20px] border-b pb-[20px]" key={reservation.id}>
-            <div className="mb-[16px] flex flex-col gap-[8px] text-[14px]">
-              <span className="min-w-[80px] text-[12px] text-grayscale-500">Number</span>
-              <span className="flex-grow text-left">{reservation.id}</span>
-            </div>
-            <div className="mb-[16px] flex flex-col gap-[8px] text-[14px]">
-              <span className="min-w-[80px] text-[12px] text-grayscale-500">Nickname</span>
-              <span className="flex-grow text-left">{reservation.users.name}</span>
-            </div>
-            <div className="mb-[16px] flex flex-col gap-[8px] text-[14px]">
-              <span className="min-w-[80px] text-[12px] text-grayscale-500">Email</span>
-              <span className="flex-grow text-left">{reservation.users.email}</span>
-            </div>
-            <div className="mb-[16px] flex flex-col gap-[8px] text-[14px]">
-              <span className="min-w-[80px] text-[12px] text-grayscale-500">Date</span>
-              <span className="flex-grow text-left">{new Date(reservation.created_at).toLocaleString()}</span>
-            </div>
-            <div className="mb-[16px] flex flex-col gap-[8px] text-[14px]">
-              <span className="min-w-[80px] text-[12px] text-grayscale-500">Tourist</span>
-              <span className="flex-grow text-left">{reservation.people}</span>
-            </div>
-            <div className="mb-[16px] flex flex-col gap-[8px] text-[14px]">
-              <span className="min-w-[80px] text-[12px] text-grayscale-500">Amount</span>
-              <span className="flex-grow text-left">{formatPrice(reservation.total_price)}</span>
-            </div>
-            <div className="mb-[16px] flex flex-col gap-[8px] text-[14px]">
-              <span className="min-w-[80px] text-[12px] text-grayscale-500">State</span>
-              <span className="flex-grow text-left">{reservation.pay_state}</span>
+          <div className="my-[20px] border-b pb-[20px] web:my-[40px] web:pb-[40px]" key={reservation.id}>
+            <div className="flex flex-col gap-[16px] web:gap-[24px]">
+              <div className="flex gap-[8px] text-[14px] mobile:flex-col web:gap-[16px] web:text-[18px]">
+                <span className="min-w-[80px] text-[12px] text-grayscale-500 web:text-[18px]">Number</span>
+                <span className="flex-grow text-left">{reservation.id}</span>
+              </div>
+              <div className="flex flex-col gap-[8px] text-[14px]">
+                <span className="min-w-[80px] text-[12px] text-grayscale-500">Nickname</span>
+                <span className="flex-grow text-left">{reservation.users.name}</span>
+              </div>
+              <div className="flex flex-col gap-[8px] text-[14px]">
+                <span className="min-w-[80px] text-[12px] text-grayscale-500">Email</span>
+                <span className="flex-grow text-left">{reservation.users.email}</span>
+              </div>
+              <div className="flex flex-col gap-[8px] text-[14px]">
+                <span className="min-w-[80px] text-[12px] text-grayscale-500">Date</span>
+                <span className="flex-grow text-left">{new Date(reservation.created_at).toLocaleString()}</span>
+              </div>
+              <div className="flex flex-col gap-[8px] text-[14px]">
+                <span className="min-w-[80px] text-[12px] text-grayscale-500">Tourist</span>
+                <span className="flex-grow text-left">{reservation.people}</span>
+              </div>
+              <div className="flex flex-col gap-[8px] text-[14px]">
+                <span className="min-w-[80px] text-[12px] text-grayscale-500">Amount</span>
+                <span className="flex-grow text-left">{formatPrice(reservation.total_price)}</span>
+              </div>
+              <div className="flex flex-col gap-[8px] text-[14px]">
+                <span className="min-w-[80px] text-[12px] text-grayscale-500">State</span>
+                <span className="flex-grow text-left">{reservation.pay_state}</span>
+              </div>
             </div>
             <button
               className="w-full rounded-lg border p-2 text-[14px] font-semibold text-grayscale-700"
