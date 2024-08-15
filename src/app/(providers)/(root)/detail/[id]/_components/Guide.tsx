bb848eb48c08184c930749c0f8b9ea32e1df39f7
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-08-15 23:08:00 +0900

    Design: 웹 버전 논의 후 수정 필요

--- src/app/(providers)/(root)/detail/[id]/_components/Guide.tsx
+++ src/app/(providers)/(root)/detail/[id]/_components/Guide.tsx
@@ -100,26 +100,27 @@ export const Guide = ({ isWeb }: WebProps) => {
   if (error) return <div>Error fetching user data</div>;
 
   return (
-    <div className="web:;gap-10 flex flex-col gap-6">
-      <div className="web:gap-6 flex flex-col gap-2">
-        <h2 className="web:text-4xl text-lg font-semibold text-grayscale-900">Contact your guide</h2>
-        <p className="web:text-lg web:mb-10 text-sm text-grayscale-500">
-          For more information such as the number of people and schedule change, it is recommended to send a message to
-          the guide to discuss and check.
-        </p>
-      </div>
-      <div className="web:flex-row flex flex-col">
-        <div className="web:flex-1 flex flex-col gap-4">
+    <div className="web:flex web:gap-10 web:flex-row web:gap-40 flex flex-col gap-6">
+      <div className="web:w-1/2 flex flex-col gap-2">
+        <div className="flex flex-col gap-4">
+          <h2 className="web:text-4xl text-lg font-semibold text-grayscale-900">Contact your guide</h2>
+          <p className="web:text-lg web:mb-10 text-sm text-grayscale-500">
+            For more information such as the number of people and schedule change, it is recommended to send a message
+            to the guide to discuss and check.
+          </p>
+        </div>
+        <div className="web:gap-6 flex flex-col gap-4">
           {user && (
-            <div className="flex flex-row items-center gap-2">
+            <div className="flex items-center gap-4">
               <Image
                 src={user.avatar}
                 alt={`${user.name}의 아바타`}
                 width={isWeb ? 80 : 56}
                 height={isWeb ? 80 : 56}
-                className="web:h-80 web:w-80 mr-2 h-14 w-14 rounded-full object-cover"
+                priority
+                className="web:h-40 web:w-40 h-14 w-14 rounded-full"
               />
-              <div className="flex flex-col gap-1">
+              <div className="flex flex-col">
                 <h4 className="web:text-2xl text-lg font-semibold">{user.name}</h4>
                 <h5 className="web:text-xl text-sm font-normal">Seoul, Korea</h5>
               </div>
@@ -133,15 +134,18 @@ export const Guide = ({ isWeb }: WebProps) => {
             Message Host
           </button>
         </div>
+      </div>
+      <div className="web:w-1/2 web:flex web:items-center web:justify-center flex hidden">
         <Image
           src="/img/GuideWeb.png"
           alt={`${user.name}의 아바타`}
-          width={isWeb ? 80 : 56}
-          height={isWeb ? 80 : 56}
-          className="web:h-80 web:w-80 web:block hidden flex-1 rounded-2xl"
+          width={624}
+          height={281}
+          sizes="100vw"
+          priority
+          className="rounded-2xl"
         />
       </div>
-      <hr className="web:my-20 mb-6 mt-3 h-[1px] w-full bg-grayscale-100" />
     </div>
   );
 };
