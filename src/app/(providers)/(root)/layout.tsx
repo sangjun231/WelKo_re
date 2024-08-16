459d239e0dbba17ce3fede714fba7220f2702b91
unknown <choipoe@gmail.com>
2024-08-17 01:22:39 +0900

    헤더 고정, 검색결과 페이지 디자인 수정

--- src/app/(providers)/(root)/layout.tsx
+++ src/app/(providers)/(root)/layout.tsx
@@ -2,11 +2,13 @@ import UpButton from '@/components/common/Button/UpButton';
 import Navbar from '@/components/common/Navbar/Navbar';
 import { PropsWithChildren, Suspense } from 'react';
 import RouteChangeHandler from './(mainpage)/_components/RouteChangeHandler';
+import Header from '@/components/common/Header/Header';
 
 function RootLayout({ children }: PropsWithChildren) {
   return (
-    <div id="root" className="web:max-w-[1440px] mx-auto flex min-h-screen w-full flex-col">
+    <div id="root" className="mx-auto flex min-h-screen w-full flex-col web:max-w-[1440px]">
       <Suspense fallback={<div>Loading...</div>}>
+        <Header />
         <RouteChangeHandler />
       </Suspense>
       <div className="relative flex-1 gap-4">
