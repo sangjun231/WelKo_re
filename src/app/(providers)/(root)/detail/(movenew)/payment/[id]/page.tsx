78688752c29fc9c9bfb791e01cb9edc23e9bdf4c
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-08-16 14:20:16 +0900

    Design: 결제 성공 페이지 웹버전 제작(피드백)

--- src/app/(providers)/(root)/detail/(movenew)/payment/[id]/page.tsx
+++ src/app/(providers)/(root)/detail/(movenew)/payment/[id]/page.tsx
@@ -5,7 +5,7 @@ import PaySuccess from '../_components/PaySuccess';
 
 export default function PaymentPage() {
   return (
-    <div className="web:px-[88px] w-full px-5">
+    <div className="w-full px-5">
       <PaySuccess />
     </div>
   );
