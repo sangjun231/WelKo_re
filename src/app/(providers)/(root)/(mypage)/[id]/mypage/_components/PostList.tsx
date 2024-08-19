c8388beaa77cb155d82ffd8d596e5ee4aa042aae
sangjunShin <987654321123sin@gmail.com>
2024-08-19 23:20:53 +0900

    Feat: 마이 페이지 투어 리스트 삭제 시 재 확인 모달 추가

--- src/app/(providers)/(root)/(mypage)/[id]/mypage/_components/PostList.tsx
+++ src/app/(providers)/(root)/(mypage)/[id]/mypage/_components/PostList.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import DeletePost from '@/hooks/Post/usePostDelete';
 import { Tables } from '@/types/supabase';
 import { API_MYPAGE_POST } from '@/utils/apiConstants';
 import { formatDateRange } from '@/utils/detail/functions';
@@ -8,6 +7,8 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import Image from 'next/image';
 import Link from 'next/link';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 import { useParams, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
@@ -25,6 +26,7 @@ const getPostsData = async (userId: string) => {
 };
 
 export default function PostList() {
+  const MySwal = withReactContent(Swal);
   const params = useParams();
   const router = useRouter();
   const userId = Array.isArray(params.id) ? params.id[0] : params.id;
@@ -53,7 +55,34 @@ export default function PostList() {
     router.push(`/${userId}/mypage/tourreservationlistpage?postId=${postId}`);
   };
 
-  const handleDelete = DeletePost();
+  const handleDelete = async (postId: string) => {
+    const result = await MySwal.fire({
+      title: 'Do you want to cancel your tour?',
+      text: 'If you cancel, you will get a full refund',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Cancel Tour',
+      cancelButtonText: 'No thanks',
+      customClass: {
+        actions: 'flex flex-col gap-[8px] w-full',
+        title: 'font-semibold text-[18px]',
+        htmlContainer: 'text-grayscale-500 text-[14px]',
+        popup: 'rounded-[16px] p-[24px]',
+        confirmButton: 'bg-primary-300 text-white w-full text-[16px] p-[12px] rounded-[12px]',
+        cancelButton: 'bg-white text-[16px] p-[12px] w-full rounded-[12px] text-grayscale-700'
+      }
+    });
+
+    if (result.isConfirmed) {
+      try {
+        await axios.delete('/api/post', { data: { post_id: postId } });
+        MySwal.fire('Deleted!', 'Your post has been deleted.', 'success');
+        router.replace('/');
+      } catch (error) {
+        MySwal.fire('Failed!', 'Failed to delete post.', 'error');
+      }
+    }
+  };
 
   useEffect(() => {
     refetch();
