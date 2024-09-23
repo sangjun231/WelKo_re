6b7c4ade454cfd4abc6bd1cdac6023e016074b0f
sangjunShin <987654321123sin@gmail.com>
2024-09-23 23:21:22 +0900

    Refactor: 결제 내역 페이지에서 해당 투어 좋아요 기능 삭제
    우선 주석 처리 , UX적으로 검토 요망

--- src/app/(providers)/(root)/detail/(movenew)/payment/_components/PaymentHistory.tsx
+++ src/app/(providers)/(root)/detail/(movenew)/payment/_components/PaymentHistory.tsx
@@ -96,7 +96,7 @@ export default function PaymentHistory() {
   const numberOfPeople = postPrice > 0 ? paymentData.total_price / postPrice : 'N/A';
 
   return (
-    <div className="web:gap-20 mt-2 flex w-full flex-col">
+    <div className="mt-2 flex w-full flex-col web:gap-20">
       <div className="web:flex web:gap-5">
         <button className="icon-button my-2" onClick={handleExitClick}>
           {isWeb ? (
@@ -105,73 +105,73 @@ export default function PaymentHistory() {
             <ExitBtn width={24} height={24} /> // 모바일 버전일 경우 ExitBtn 사용
           )}
         </button>
-        <h1 className="web:text-3xl my-5 text-xl font-semibold">Reservation confirmed</h1>
+        <h1 className="my-5 text-xl font-semibold web:text-3xl">Reservation confirmed</h1>
       </div>
       {post && (
         <div>
           <div className="flex items-start space-x-4">
             {/* 이미지와 제목 */}
-            <div className="web:h-[120px] web:w-[120px] relative h-20 w-20 flex-shrink-0 overflow-hidden rounded-lg">
+            <div className="relative h-20 w-20 flex-shrink-0 overflow-hidden rounded-lg web:h-[120px] web:w-[120px]">
               <Image src={post.image} alt={post.title} fill style={{ objectFit: 'cover' }} className="rounded-lg" />
               <button
                 onClick={handleLike}
                 className="absolute right-1 top-2 rounded-full bg-[rgba(255,255,255,0.10)] p-0.5 backdrop-blur-[10px]"
               >
-                {liked ? (
+                {/* {liked ? (
                   <LikeBtn width={20} height={20} color="#FF7029" fill="#FF7029" />
                 ) : (
                   <LikeBtn width={20} height={20} color="white" />
-                )}
+                )} */}
               </button>
             </div>
             <div className="flex flex-col gap-1">
-              <h4 className="web:text-xl line-clamp-1 text-sm font-semibold">{post.title}</h4>
-              <p className="web:text-lg text-sm text-gray-500">{formatDateRange(post.startDate, post.endDate)}</p>
-              <div className="web:text-lg text-sm font-medium">
+              <h4 className="line-clamp-1 text-sm font-semibold web:text-xl">{post.title}</h4>
+              <p className="text-sm text-gray-500 web:text-lg">{formatDateRange(post.startDate, post.endDate)}</p>
+              <div className="text-sm font-medium web:text-lg">
                 <span className="text-primary-300">${post.price}</span>
                 <span className="text-grayscale-700">/Person</span>
               </div>
             </div>
           </div>
-          <hr className="web:my-10 my-5 h-[1px] w-full bg-grayscale-100" />
+          <hr className="my-5 h-[1px] w-full bg-grayscale-100 web:my-10" />
           {/* 예약 정보 */}
 
           <div className="flex flex-col gap-1">
-            <h3 className="web:text-3xl text-lg font-semibold">Reservation</h3>
+            <h3 className="text-lg font-semibold web:text-3xl">Reservation</h3>
             <div className="mt-2 flex flex-col gap-2">
-              <span className="web:text-lg text-xs text-grayscale-500">Number</span>
-              <span className="web:text-lg text-sm text-grayscale-900">{paymentData.id}</span>
+              <span className="text-xs text-grayscale-500 web:text-lg">Number</span>
+              <span className="text-sm text-grayscale-900 web:text-lg">{paymentData.id}</span>
             </div>
             <div className="mt-2 flex flex-col gap-2">
-              <span className="web:text-lg text-xs text-grayscale-500">Nickname</span>
-              <span className="web:text-lg text-sm text-grayscale-900">{paymentData.users.name}</span>
+              <span className="text-xs text-grayscale-500 web:text-lg">Nickname</span>
+              <span className="text-sm text-grayscale-900 web:text-lg">{paymentData.users.name}</span>
             </div>
             <div className="mt-2 flex flex-col gap-2">
-              <span className="web:text-lg text-xs text-grayscale-500">Email</span>
-              <span className="web:text-lg text-sm text-grayscale-900">{paymentData.users.email}</span>
+              <span className="text-xs text-grayscale-500 web:text-lg">Email</span>
+              <span className="text-sm text-grayscale-900 web:text-lg">{paymentData.users.email}</span>
             </div>
             <div className="mt-2 flex flex-col gap-2">
-              <span className="web:text-lg text-xs text-grayscale-500">Tourist</span>
-              <span className="web:text-lg text-sm text-grayscale-900">{numberOfPeople}</span>
+              <span className="text-xs text-grayscale-500 web:text-lg">Tourist</span>
+              <span className="text-sm text-grayscale-900 web:text-lg">{numberOfPeople}</span>
             </div>
           </div>
 
-          <hr className="web:my-10 my-5 h-[1px] w-full bg-grayscale-100" />
+          <hr className="my-5 h-[1px] w-full bg-grayscale-100 web:my-10" />
 
           {/* 결제 정보 */}
           <div className="flex flex-col gap-1">
-            <h3 className="web:text-3xl text-lg font-semibold">Payment</h3>
+            <h3 className="text-lg font-semibold web:text-3xl">Payment</h3>
             <div className="mt-2 flex flex-col gap-2 text-sm">
-              <span className="web:text-lg text-xs text-grayscale-500">Date</span>
+              <span className="text-xs text-grayscale-500 web:text-lg">Date</span>
               <span className="text-sm text-grayscale-900">{formatDate(paymentData.created_at)}</span>
             </div>
             <div className="mt-2 flex flex-col gap-2">
-              <span className="web:text-lg text-xs text-grayscale-500">Amount</span>
-              <span className="web:text-lg text-sm text-grayscale-900">${paymentData.total_price.toFixed(2)}</span>
+              <span className="text-xs text-grayscale-500 web:text-lg">Amount</span>
+              <span className="text-sm text-grayscale-900 web:text-lg">${paymentData.total_price.toFixed(2)}</span>
             </div>
             <div className="mt-2 flex flex-col gap-2">
-              <span className="web:text-lg text-xs text-grayscale-500">Method</span>
-              <span className="web:text-lg text-sm text-grayscale-900">Card</span>
+              <span className="text-xs text-grayscale-500 web:text-lg">Method</span>
+              <span className="text-sm text-grayscale-900 web:text-lg">Card</span>
             </div>
           </div>
         </div>
