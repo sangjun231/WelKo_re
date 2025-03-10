646cbef0aa3f4857545feadb820fa796ec21f941
sangjun231 <44526444+sangjun231@users.noreply.github.com>
2025-03-10 10:58:02 +0900

    Refactor: 마이페이지 좋아요 리스트 개선
    마이페이지 좋아요 리스트 store를 통해 관리되도록 개선

--- src/app/(providers)/(root)/(mypage)/[id]/mypage/_components/Like.tsx
+++ src/app/(providers)/(root)/(mypage)/[id]/mypage/_components/Like.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useQuery, useMutation } from '@tanstack/react-query';
+import React from 'react';
 import Image from 'next/image';
-import { API_MYPAGE_LIKES } from '@/utils/apiConstants';
+import { useLikeStore } from '@/zustand/likeStore';
 
 type LikeProps = {
   postId: string;
@@ -12,59 +10,36 @@ type LikeProps = {
 };
 
 const Like: React.FC<LikeProps> = ({ postId, userId }) => {
-  const [liked, setLiked] = useState(false);
+  const { isLiked, fetchLikeStatus, toggleLike } = useLikeStore((state) => ({
+    isLiked: state.isLiked,
+    fetchLikeStatus: state.fetchLikeStatus,
+    toggleLike: state.toggleLike
+  }));
 
-  const fetchLikeStatus = async (): Promise<boolean> => {
-    const response = await axios.get(API_MYPAGE_LIKES(userId));
-    const likedPosts = response.data.posts.map((post: any) => post.id);
-    return likedPosts.includes(postId);
-  };
-
-  const toggleLikeStatus = async (): Promise<void> => {
-    await axios.put(API_MYPAGE_LIKES(userId), { postId });
-  };
-
-  const { data, isError, isLoading, refetch } = useQuery<boolean>({
-    queryKey: ['likeStatus', postId, userId],
-    queryFn: fetchLikeStatus,
-    enabled: !!postId && !!userId
-  });
-
-  const likeMutation = useMutation<void, Error>({
-    mutationFn: toggleLikeStatus,
-    onSuccess: () => {
-      refetch();
+  React.useEffect(() => {
+    if (postId && userId) {
+      fetchLikeStatus(postId, userId);
     }
-  });
+  }, [postId, userId, fetchLikeStatus]);
 
   const handleLike = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.stopPropagation();
-
-    if (isLoading) return;
-
-    likeMutation.mutate();
-  };
-
-  useEffect(() => {
-    if (data !== undefined) {
-      setLiked(data);
+    if (postId && userId) {
+      toggleLike(postId, userId);
     }
-  }, [data]);
-
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error fetching like status</div>;
+  };
 
   return (
     <button
-      className="web:right-[16px] web:top-[16px] absolute right-[4px] top-[8px] rounded-[24px] backdrop-blur-[10px]"
+      className="absolute right-[4px] top-[8px] rounded-[24px] backdrop-blur-[10px] web:right-[16px] web:top-[16px]"
       onClick={handleLike}
       style={{
         backgroundColor: 'rgba(255, 255, 255, 0.10)'
       }}
     >
       <Image
-        className="web:w-[44px] web:h-[44px] h-[24px] w-[24px]"
-        src={liked ? '/icons/tabler-icon-heart-filled.svg' : '/icons/tabler-icon-post-heart.svg'}
+        className="h-[24px] w-[24px] web:h-[44px] web:w-[44px] web:p-[6px]"
+        src={isLiked(postId) ? '/icons/tabler-icon-heart-filled.svg' : '/icons/tabler-icon-post-heart.svg'}
         alt="heart icon"
         width={24}
         height={24}
